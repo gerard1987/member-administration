2728279efdbeaf7b9dbeef960e1e3c70eab4419f
DESKTOP-NTB6DL0\katan <gerard.taa@gmail.com>
2024-10-15 20:46:49 +0200

    Authentication

--- routes/web.php
+++ routes/web.php
@@ -9,20 +9,20 @@ Route::get('/login', [AuthController::class, 'login_index'])->name('login');
 Route::post('/login', [AuthController::class, 'login'])->name('login.submit');
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
 
-// Show the registration form
 Route::get('/register', [AuthController::class, 'register_index'])->name('register');
 Route::post('/register', [AuthController::class, 'register'])->name('register.submit');
-
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-Route::get('/', [FamilyController::class, 'index']);
-Route::get('/families', [FamilyController::class, 'index'])->name('families.index');
-Route::get('/families/{id}', [FamilyController::class, 'view'])->name('families.view');
-Route::post('/families', [FamilyController::class, 'create'])->name('families.create');
-Route::post('/families/{id}', [FamilyController::class, 'edit'])->name('families.edit');
-Route::delete('/families/{id}', [FamilyController::class, 'delete'])->name('families.delete');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', [FamilyController::class, 'index']);
+    Route::get('/families', [FamilyController::class, 'index'])->name('families.index');
+    Route::get('/families/{id}', [FamilyController::class, 'view'])->name('families.view');
+    Route::post('/families', [FamilyController::class, 'create'])->name('families.create');
+    Route::post('/families/{id}', [FamilyController::class, 'edit'])->name('families.edit');
+    Route::delete('/families/{id}', [FamilyController::class, 'delete'])->name('families.delete');
 
-Route::get('/families/{family_id}/members/{member_id}', [FamilyMemberController::class, 'view'])->name('families.members.view');
-Route::post('/families/{id}/family_member', [FamilyMemberController::class, 'create'])->name('families.create.member');
-Route::post('/families/{family_id}/members/{member_id}/edit', [FamilyMemberController::class, 'edit'])->name('families.members.edit');
-Route::delete('/families/{family_id}/members/{member_id}', [FamilyMemberController::class, 'delete'])->name('families.members.delete');
+    Route::get('/families/{family_id}/members/{member_id}', [FamilyMemberController::class, 'view'])->name('families.members.view');
+    Route::post('/families/{id}/family_member', [FamilyMemberController::class, 'create'])->name('families.create.member');
+    Route::post('/families/{family_id}/members/{member_id}/edit', [FamilyMemberController::class, 'edit'])->name('families.members.edit');
+    Route::delete('/families/{family_id}/members/{member_id}', [FamilyMemberController::class, 'delete'])->name('families.members.delete');
+});
